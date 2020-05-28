50e735484be10359453eba9e3091fa863843f2d7
JeqnBat <jbpellier@noos.fr>
2020-05-28 14:52:02 +0200

    Ref #1 - Commenting 90% done

--- js/template.js
+++ js/template.js
@@ -1,7 +1,8 @@
 /*jshint laxbreak:true */
 (function (window) {
 	'use strict';
-// PROBING APP ________________________________________ */
+
+// Objet htmlEscapes stocke des valeurs html correspondant à leur caractère UTF8
 	var htmlEscapes = {
 		'&': '&amp;',
 		'<': '&lt;',
@@ -11,6 +12,7 @@
 		'`': '&#x60;'
 	};
 
+// Variable qui sert de sélecteur à l'objet htmlEscapes
 	var escapeHtmlChar = function (chr) {
 		return htmlEscapes[chr];
 	};
@@ -18,18 +20,28 @@
 	var reUnescapedHtml = /[&<>"'`]/g;
 	var reHasUnescapedHtml = new RegExp(reUnescapedHtml.source);
 
+// L'opérateur (ternaire) conditionnel est le seul opérateur JavaScript
+// qui comporte trois opérandes. Cet opérateur est fréquemment utilisé comme
+// raccourci pour la déclaration de Instructions/if...else.
+// ici on teste pour savoir si un caractère spécial doit être remplacé dans le DOM
 	var escape = function (string) {
+		// condition ? exprSiVrai : exprSiFaux
+		// teste 'string & reHasUnescapedHtml.test(string)'
 		return (string && reHasUnescapedHtml.test(string))
+			// Écrire après un return ? Pq c'est censé être sur la même ligne : opérateur conditionnel
+			// si le test renvoie true alors remplacer la string
 			? string.replace(reUnescapedHtml, escapeHtmlChar)
+			// si renvoie faux alors ne pas changer la string
 			: string;
 	};
 
+// OBJET Template _____________________________________ */
+// 1. Prépare des valeurs par défaut pour toutes les méthodes de Template, comme un template par défaut (?)
 	/**
-	 * Sets up defaults for all the Template methods such as a default template
-	 *
 	 * @constructor
 	 */
 	function Template() {
+		// apparence de la liste contenant les Todos
 		this.defaultTemplate
 		=	'<li data-id="{{id}}" class="{{completed}}">'
 		+		'<div class="view">'
@@ -39,16 +51,15 @@
 		+		'</div>'
 		+	'</li>';
 	}
-
+// MÉTHODES (3) _______________________________________ */
+// I. template.show(data)
+// 1. Crée un string HTML avec <li>
+// 2. Renvoie ce string dans l'appli
+// Note: En réalité il faudrait utiliser un moteur de templating tel que Mustache
+//			 ou Handlebars, toutefois, ceci est un exemple vanilla js.
 	/**
-	 * Creates an <li> HTML string and returns it for placement in your app.
-	 *
-	 * NOTE: In real life you should be using a templating engine such as Mustache
-	 * or Handlebars, however, this is a vanilla JS example.
-	 *
-	 * @param {object} data The object containing keys you want to find in the
-	 *                      template to replace.
-	 * @returns {string} HTML String of an <li> element
+	 * @param {object} data Objet contenant les valeurs que l'on souhaite remplacer
+	 * @returns {string} String d'un élément HTML de type <li>
 	 *
 	 * @example
 	 * view.show({
@@ -60,14 +71,16 @@
 	Template.prototype.show = function (data) {
 		var i, l;
 		var view = '';
-
+		// pour chaque membre de data
 		for (i = 0, l = data.length; i < l; i++) {
 			var template = this.defaultTemplate;
 			var completed = '';
 			var checked = '';
-
+			// si la propriété 'completed' de data[i] est TRUE alors
 			if (data[i].completed) {
+				// modifier la variable completed
 				completed = 'completed';
+				// modifier la variable checked
 				checked = 'checked';
 			}
 			// remplace les placeholders par les vraies valeurs
@@ -81,24 +94,23 @@
 
 		return view;
 	};
-
+// II. template.itemCounter(activeTodos)
+// 1. Affiche un compteur du nombre de Todos à compléter
 	/**
-	 * Displays a counter of how many to dos are left to complete
-	 *
-	 * @param {number} activeTodos The number of active todos.
-	 * @returns {string} String containing the count
+	 * @param {number} activeTodos Le nombre des todos actifs.
+	 * @returns {string} un string contenant le nombre.
 	 */
 	Template.prototype.itemCounter = function (activeTodos) {
+		// si activeTodos = 1 alors plural est vide, sinon il équivaut à la string 's'
 		var plural = activeTodos === 1 ? '' : 's';
-
+		// retourne activeTodos en BOLD + rajoute un 's' à 'item' si jamais il y a + d'un activeTodo
 		return '<strong>' + activeTodos + '</strong> item' + plural + ' left';
 	};
-
+// III. template.clearCompletedButton(completedTodos)
+// 1. Met à jour le text à l'intérieur du bouton "clear completed"
 	/**
-	 * Updates the text within the "Clear completed" button
-	 *
-	 * @param  {[type]} completedTodos The number of completed todos.
-	 * @returns {string} String containing the count
+	 * @param  {[type]} completedTodos le nombre de todos complétés
+	 * @returns {string} un string contenant ce nombre
 	 */
 	Template.prototype.clearCompletedButton = function (completedTodos) {
 		if (completedTodos > 0) {
--- js/view.js
+++ js/view.js
@@ -4,22 +4,17 @@
 
 // OBJET View _________________________________________ */
 // 1. L'objet View représente le DOM
-// 2. Il a 2 points d'entrée
-	/**
-	 * View that abstracts away the browser's DOM completely.
-	 * It has two simple entry points:
-	 *
-	 *   - bind(eventName, handler)
-	 *     Takes a todo application event and registers the handler
-	 *   - render(command, parameterObject)
-	 *     Renders the given command with the options
-	 */
+// 2. Il a 2 points d'entrée :
+//			a. bind(eventName, handler)
+//			   Prend un event et enregistre le handler
+//			b. render(command, parameterObject)
+//				 Effectue la commande appelée avec les options
 	function View(template) {
 		this.template = template;
 
 		this.ENTER_KEY = 13;
 		this.ESCAPE_KEY = 27;
-
+		// 'qs' défini dans 'helpers.js' -> qs(selector, scope)
 		this.$todoList = qs('.todo-list');
 		this.$todoItemCounter = qs('.todo-count');
 		this.$clearCompleted = qs('.clear-completed');
@@ -28,56 +23,71 @@
 		this.$toggleAll = qs('.toggle-all');
 		this.$newTodo = qs('.new-todo');
 	}
-	
-// MÉTHODES ___________________________________________ */
+// MÉTHODES ( ) _______________________________________ */
+// I. view._removeItem(id)
+// 1. Retire un élément de la <li> grâce à son id
 	View.prototype._removeItem = function (id) {
 		var elem = qs('[data-id="' + id + '"]');
-
+		// si 'elem' existe
 		if (elem) {
+			// retirer 'elem' de '$todolist'
 			this.$todoList.removeChild(elem);
 		}
 	};
-
+// II. view._clearCompletedButton(completedCount, visible)
+// 1.
 	View.prototype._clearCompletedButton = function (completedCount, visible) {
 		this.$clearCompleted.innerHTML = this.template.clearCompletedButton(completedCount);
+		// si 'visible' est true -> display:block; else, display:none;
 		this.$clearCompleted.style.display = visible ? 'block' : 'none';
 	};
-
+// III. view._setFilter(currentPage)
 	View.prototype._setFilter = function (currentPage) {
+		// sélectionne les éléments de classe filters dans le scope selected et
+		// les vide de leurs classes
 		qs('.filters .selected').className = '';
+		// sélectionne les éléments de classe filters dans le scope [href="#/' + currentPage + '"]
+		// et leur affecte la classe 'selected'
 		qs('.filters [href="#/' + currentPage + '"]').className = 'selected';
 	};
-
+// IV. view._elementComplete(id, completed)
+// 1.
 	View.prototype._elementComplete = function (id, completed) {
 		var listItem = qs('[data-id="' + id + '"]');
-
+		// si 'listItem' n'existe pas
 		if (!listItem) {
 			return;
 		}
-
+		// si 'listItem' existe
+			// si completed existe alors className de listItem = 'completed', sinon className = ''
 		listItem.className = completed ? 'completed' : '';
 
 		// In case it was toggled from an event and not by clicking the checkbox
 		qs('input', listItem).checked = completed;
 	};
-
+// V. view._editItem(id, title)
+// 1.
 	View.prototype._editItem = function (id, title) {
+		// 'listItem' stocke l'élément du DOM dont l'id a été appelé
 		var listItem = qs('[data-id="' + id + '"]');
-
+		// si 'listItem' n'existe pas : return
 		if (!listItem) {
 			return;
 		}
-
+		// Sinon, modifie le className de listItem	en ajoutant la string ' editing'
 		listItem.className = listItem.className + ' editing';
-
+		// crée une variable input qui stocke un élément créé du DOM et appelé 'input'
 		var input = document.createElement('input');
+		// ajoute la classe 'edit' à cet élément
 		input.className = 'edit';
-
+		// ajoute input à listItem
 		listItem.appendChild(input);
+		// focus input
 		input.focus();
+		// passer 'title' comme valeur de input
 		input.value = title;
 	};
-
+// VI. view._editItemDone(id, title)
 	View.prototype._editItemDone = function (id, title) {
 		var listItem = qs('[data-id="' + id + '"]');
 
@@ -94,9 +104,12 @@
 			label.textContent = title;
 		});
 	};
-
+// VII. view.render(viewCmd, parameter)
+// 1. Distribue les méthodes précédentes par le biais d'un objet (viewCommands)
 	View.prototype.render = function (viewCmd, parameter) {
 		var self = this;
+		// crée un objet 'viewCommands'
+		// chaque clef de l'objet représente une commande, chaque valeur, l'action à effectuer lorsque la commande est appelée
 		var viewCommands = {
 			showEntries: function () {
 				self.$todoList.innerHTML = self.template.show(parameter);
@@ -132,15 +145,15 @@
 				self._editItemDone(parameter.id, parameter.title);
 			}
 		};
-
+		// lors de l'appel, un chiffre remplace viewCmd
 		viewCommands[viewCmd]();
 	};
-
+// VIII. view_editItemDone
 	View.prototype._itemId = function (element) {
 		var li = $parent(element, 'li');
 		return parseInt(li.dataset.id, 10);
 	};
-
+// IX. view._bindItemEditDone(handler)
 	View.prototype._bindItemEditDone = function (handler) {
 		var self = this;
 		$delegate(self.$todoList, 'li .edit', 'blur', function () {
@@ -160,7 +173,7 @@
 			}
 		});
 	};
-
+// X. view._bindItemEditCancel(handler)
 	View.prototype._bindItemEditCancel = function (handler) {
 		var self = this;
 		$delegate(self.$todoList, 'li .edit', 'keyup', function (event) {
@@ -172,7 +185,7 @@
 			}
 		});
 	};
-
+// XI. view.bind(event, handler)
 	View.prototype.bind = function (event, handler) {
 		var self = this;
 		if (event === 'newTodo') {
